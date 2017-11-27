c7d5d1c1b7914e56e7d74a9b05e0c34ae2582f66
Brett Ausmeier <brett.ausmeier@jembi.org>
2017-11-27 15:24:40 +0200

    Add simple export page
    To simplify the export process for the user.
    
    MOZDEV-38

--- mediator/request-router.js
+++ mediator/request-router.js
@@ -5,8 +5,11 @@ const cors = require('cors')
 const dhisHandler = require('./sync-handlers/dhis-handler')
 const pentahoHandler = require('./sync-handlers/pentaho-handler')
 const exportHandler = require('./export-handler')
+const request = require('request-promise-native')
 
 const router = Router()
+
+router.use(cors())
 
 router.get(
     '/dhis/test',
@@ -16,10 +19,36 @@ router.get(
     '/pentaho/test',
     pentahoHandler.handleRequest
 )
+
 router.get(
   '/export',
-  cors(),
   exportHandler.handleExport
+)
+
+router.get(
+  '/orgUnits',
+  (req, res, next) => {
+    request({
+      method: 'GET',
+      url: 'http://admin:district@dhis-web:8080/api/organisationUnits?paging=false',
+      json: true
+    }).then((response) => {
+      res.status(200).send(response.organisationUnits)
+    }).catch(next)
+  }
+)
+
+router.get(
+  '/dataSets',
+  (req, res, next) => {
+    request({
+      method: 'GET',
+      url: 'http://admin:district@dhis-web:8080/api/dataSets?paging=false',
+      json: true
+    }).then((response) => {
+      res.status(200).send(response.dataSets)
+    }).catch(next)
+  }
 )
 
 module.exports = exports = router
