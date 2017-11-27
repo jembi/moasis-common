ed62c5baca3183768dd45b3af55a640f462b7b3e
Nathan Floor <nathan.floor@jembi.org>
2017-11-27 10:09:51 +0200

    MOZDEV-36: corrected compsoe file

--- mediator/database.js
+++ mediator/database.js
@@ -6,7 +6,7 @@ exports.initialize = () => {
   client.connect()
 
   client.query('CREATE TABLE IF NOT EXISTS MD_Facilities(id integer, name varchar(255), uuid varchar(255), properties text, lat decimal, lng decimal)')
-    .then(res => console.log('MD_Facilities table created'))
+    .then(res => console.log('MD_Facilities table created...'))
     .catch(err => console.error(err.stack))
   // client.query('CREATE TABLE IF NOT EXISTS DataValues()')
 }
